cf455523264e75365b35f0a05445a98215201bdc
Rhys van der Waerden <rhys.vdw@gmail.com>
2018-08-19 20:30:25 +1000

    Correctly import generated type guards
    Closes #12

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import Project, { InterfaceDeclaration, PropertySignature, Type, TypeGuards, SymbolFlags, JSDoc, ExportableNode, Node, SourceFile } from 'ts-simple-ast'
+import Project, { InterfaceDeclaration, PropertySignature, Type, TypeGuards, JSDoc, ExportableNode, Node, SourceFile } from 'ts-simple-ast'
 import { flatMap } from "lodash"
 
 // -- Types --
@@ -20,25 +20,16 @@ function findExportableNode(type: Type): ExportableNode & Node | null {
     return flatMap(
         symbol.getDeclarations(),
         d => [d, ...d.getAncestors()]
-    ).find(TypeGuards.isExportableNode) || null
+    ).filter(TypeGuards.isExportableNode).find(n => n.isExported()) || null
 }
 
 function typeToDependency(type: Type): Dependency | null {
     const exportable = findExportableNode(type)
     if (exportable === null) {
-        return exportable
-    }
-
-    const sourceFile = exportable.getAncestors().find(node => {
-        const symbol = node.getSymbol()
-        return symbol !== undefined && (symbol.getFlags() & SymbolFlags.Module) !== 0
-    }) as SourceFile
-
-    if (sourceFile === undefined) {
-        // This is a global (I think).
         return null
     }
 
+    const sourceFile = exportable.getSourceFile()
     const name = exportable.getSymbol()!.getName()
     const isDefault = exportable.isDefaultExport()
 
@@ -141,13 +132,13 @@ function typeOf(varName: string, type: string): string {
     return eq(`typeof ${varName}`, `"${type}"`)
 }
 
-function typeUnionConditions(varName: string, types: Type[], isOptional: boolean, dependencies: Dependency[]): string {
+function typeUnionConditions(varName: string, types: Type[], isOptional: boolean, dependencies: Dependency[], project: Project): string {
     const conditions: string[] = []
     if (isOptional && types.findIndex(type => type.isUndefined()) === -1) {
         conditions.push(typeOf(varName, "undefined"))
     }
     conditions.push(...types
-        .map(type => typeConditions(varName, type, false, dependencies))
+        .map(type => typeConditions(varName, type, false, dependencies, project))
         .filter((v) => v !== null) as string[]
     )
     return parens(ors(...conditions))
@@ -157,21 +148,23 @@ function parens(code: string) {
     return `(\n${indent(code, 1)}\n)`
 }
 
-function arrayCondition(varName: string, arrayType: Type, dependencies: Dependency[]): string {
+function arrayCondition(varName: string, arrayType: Type, dependencies: Dependency[], project: Project): string {
     if (arrayType.getText() === "never") return ands(
             `Array.isArray(${varName})`,
             eq(`${varName}.length`, '0'),
     )
     return ands (
         `Array.isArray(${varName})`,
-        `${varName}.every(e => ${typeConditions("e", arrayType, false, dependencies)})`
+        `${varName}.every(e =>\n${indent(typeConditions("e", arrayType, false, dependencies, project)!, 1)}\n)`
     )
 }
 
-function typeConditions(varName: string, type: Type, isOptional: boolean = false, dependencies: Dependency[]): string | null {
-    const dependency = typeToDependency(type)
-    if (dependency !== null) {
-        dependencies.push(dependency)
+function typeConditions(varName: string, type: Type, isOptional: boolean, dependencies: Dependency[], project: Project): string | null {
+    function addDependency(type: Type) {
+        const dependency = typeToDependency(type)
+        if (dependency !== null) {
+            dependencies.push(dependency)
+        }
     }
     if (type.getText() === "any") {
         return null
@@ -180,27 +173,58 @@ function typeConditions(varName: string, type: Type, isOptional: boolean = false
         return typeOf(varName, "undefined")
     }
     if (type.isUnion()) {
-        return typeUnionConditions(varName, type.getUnionTypes(), isOptional, dependencies)
+        // Seems to be bug here where enums can only be detected with enum
+        // literal + union check... odd.
+        if (type.isEnumLiteral()) {
+            addDependency(type)
+        }
+        return typeUnionConditions(varName, type.getUnionTypes(), isOptional, dependencies, project)
     }
     if (type.isIntersection()) {
-        return typeUnionConditions(varName, type.getIntersectionTypes(), isOptional, dependencies)
+        return typeUnionConditions(varName, type.getIntersectionTypes(), isOptional, dependencies, project)
     }
     if (isOptional) {
-        return typeUnionConditions(varName, [type], isOptional, dependencies)
+        return typeUnionConditions(varName, [type], isOptional, dependencies, project)
     }
     if (type.isArray()) {
-        return arrayCondition(varName, type.getArrayType()!, dependencies)
+        return arrayCondition(varName, type.getArrayType()!, dependencies, project)
     }
     if (isReadonlyArrayType(type)) {
-        return arrayCondition(varName, getReadonlyArrayType(type)!, dependencies)
+        return arrayCondition(varName, getReadonlyArrayType(type)!, dependencies, project)
     }
     if (isClassType(type)) {
+        addDependency(type)
         return `${varName} instanceof ${type.getSymbol()!.getName()}`
     }
     if (type.isInterface()) {
-        return `${isInterfaceFunctionNames.get(type)}(${varName})`
+        const declarations = type.getSymbol()!.getDeclarations();
+        const docs = flatMap(declarations, d =>
+            TypeGuards.isJSDocableNode(d) ? d.getJsDocs() : []
+        )
+        const declaration = declarations.find(TypeGuards.isInterfaceDeclaration);
+        if (declaration === undefined) {
+            console.error(`ERROR: Couldn't find declaration for type ${type.getText()}`)
+            return null
+        }
+        const typeGuardName = getTypeGuardName(declaration.getName(), docs)
+        if (typeGuardName === null) {
+            return objectConditions(varName, declaration.getProperties(), dependencies, project)
+        }
+
+        // TODO: This line returns the path lower cased.
+        // https://github.com/dsherret/ts-simple-ast/issues/394
+        const sourcePath = declaration.getSourceFile()!.getFilePath()
+
+        dependencies.push({
+            name: typeGuardName,
+            sourceFile: findOrCreate(project, outFilePath(sourcePath)),
+            isDefault: false
+        })
+
+        return `${typeGuardName}(${varName})`
     }
     if (type.isObject()) {
+        // TODO: Assert object interface
         return typeOf(varName, "object")
     }
     if (type.isLiteral()) {
@@ -209,38 +233,48 @@ function typeConditions(varName: string, type: Type, isOptional: boolean = false
     return typeOf(varName, type.getText())
 }
 
-function propertyConditions(property: PropertySignature, dependencies: Dependency[]): string | null {
-    const varName = `obj.${property.getName()}`;
-    return typeConditions(varName, property.getType(), property.hasQuestionToken(), dependencies)
+function propertyConditions(objName: string, property: PropertySignature, dependencies: Dependency[], project: Project): string | null {
+    const varName = `${objName}.${property.getName()}`;
+    return typeConditions(varName, property.getType(), property.hasQuestionToken(), dependencies, project)
 }
 
-function propertiesConditions(properties: ReadonlyArray<PropertySignature>, dependencies: Dependency[]): string[] {
-    return properties.map(prop => propertyConditions(prop, dependencies)).filter(v => v !== null) as string[]
+function propertiesConditions(varName: string, properties: ReadonlyArray<PropertySignature>, dependencies: Dependency[], project: Project): string[] {
+    return properties.map(prop => propertyConditions(varName, prop, dependencies, project)).filter(v => v !== null) as string[]
 }
 
-const isInterfaceFunctionNames = new WeakMap<Type, string>()
+function objectConditions(varName: string, properties: ReadonlyArray<PropertySignature>, dependencies: Dependency[], project: Project): string {
+    return ands(
+        typeOf(varName, "object"),
+        ...propertiesConditions(varName, properties, dependencies, project)
+    )
+}
 
-function generateTypeGuard(functionName: string, iface: InterfaceDeclaration, dependencies: Dependency[]): string {
-    const type = iface.getType();
-    isInterfaceFunctionNames.set(type, functionName);
+function generateTypeGuard(functionName: string, iface: InterfaceDeclaration, dependencies: Dependency[], project: Project): string {
+    const conditions = objectConditions('obj', iface.getProperties(), dependencies, project)
 
-    // TODO: Assert object interface
-
-    const conditions: string[] = [
-        typeOf('obj', "object"),
-        ...propertiesConditions(iface.getProperties(), dependencies)
-    ]
-
-    return `
+return `
 export function ${functionName}(obj: any): obj is ${iface.getName()} {
     return (
-${indent(ands(...conditions), 2)}
-    );
+${indent(conditions, 2)}
+    )
 }
 `
 }
 
 // -- Process project --
+
+function findOrCreate(project: Project, path: string): SourceFile {
+    let outFile = project.getSourceFile(path)
+    if (outFile === undefined) {
+        outFile = project.createSourceFile(path)
+    }
+    return outFile
+ }
+
+
+function clearOrCreate(project: Project, path: string): SourceFile {
+    return project.createSourceFile(path, "", { overwrite: true })
+ }
 
 export function generate(paths: string[]) {
     const project = new Project()
@@ -258,7 +292,7 @@ export function generate(paths: string[]) {
                         isDefault: iface.isDefaultExport(),
                         sourceFile,
                     })
-                    acc.push(generateTypeGuard(typeGuardName, iface, dependencies))
+                    acc.push(generateTypeGuard(typeGuardName, iface, dependencies, project))
                 } else {
                     console.error(
                         `ERROR: interface ${iface.getName()} is not exported, ` +
@@ -271,12 +305,7 @@ export function generate(paths: string[]) {
 
         if (functions.length > 0) {
             const outPath = outFilePath(sourceFile.getFilePath())
-            let outFile = project.getSourceFile(outPath)
-            if (outFile) {
-                outFile.removeText()
-            } else {
-                outFile = project.createSourceFile(outPath)
-            }
+            const outFile = clearOrCreate(project, outPath)
             outFile.addStatements(functions.join('\n'))
 
             // Dedupe imports
@@ -296,13 +325,16 @@ export function generate(paths: string[]) {
                 return acc
             }, new Map<SourceFile, { default: string | undefined, named: Set<string> }>())
 
-            imports.forEach((value, path) => {
-                outFile!.addImportDeclaration({
-                    defaultImport: value.default,
-                    moduleSpecifier: sourceFile.getRelativePathAsModuleSpecifierTo(path),
-                    namedImports: Array.from(value.named),
-                })
-            })
+            for (const [importFile, { default: defaultImport, named }] of imports.entries()) {
+                // Don't self-import
+                if (importFile !== outFile) {
+                    outFile.addImportDeclaration({
+                        defaultImport,
+                        moduleSpecifier: sourceFile.getRelativePathAsModuleSpecifierTo(importFile),
+                        namedImports: Array.from(named),
+                    })
+                }
+            }
         }
     })
 
