1798d836f2fa0cbf536374222795662f53a21f96
Rhys van der Waerden <rhys.vdw@gmail.com>
2018-11-26 17:49:45 +1100

    Add usage guide

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,7 @@
 /* tslint:disable:no-console */
 
 import commandLineArgs from 'command-line-args'
+import commandLineUsage from 'command-line-usage'
 import { defaults } from 'lodash'
 import { FileNotFoundError } from 'ts-simple-ast'
 import * as TsConfig from 'tsconfig'
@@ -12,15 +13,42 @@ interface ICliOptions {
   shortcircuit: string | undefined
   paths: ReadonlyArray<string>
   project: string | undefined
+  help: boolean
 }
 
+const optionList = [
+  {
+    description:
+      'A JavaScript condition used to automatically return `true` from guard functions, bypassing checks. eg. `process.env.DEBUG === "production"`',
+    name: 'shortcircuit',
+    type: String,
+    typeLabel: '{underline javascript}',
+  },
+  {
+    defaultOption: true,
+    description:
+      'File(s) to generate guard for. If excluded this will process {italic all} project files.',
+    multiple: true,
+    name: 'paths',
+    type: String,
+    typeLabel: '{underline file[]} ...',
+  },
+  {
+    description: 'Path to tsconfig.json',
+    name: 'project',
+    type: String,
+    typeLabel: '{underline file}',
+  },
+  {
+    alias: 'h',
+    description: 'Print this usage guide',
+    name: 'help',
+    type: Boolean,
+  },
+]
 const options: ICliOptions = defaults(
-  commandLineArgs([
-    { name: 'shortcircuit', type: String },
-    { name: 'paths', type: String, multiple: true, defaultOption: true },
-    { name: 'project', type: String },
-  ]) as ICliOptions,
-  { paths: [] as ReadonlyArray<string> }
+  commandLineArgs(optionList) as ICliOptions,
+  { paths: [] as ReadonlyArray<string>, help: false }
 )
 
 async function run() {
@@ -47,4 +75,24 @@ async function run() {
   }
 }
 
-run()
+if (options.help) {
+  // tslint:disable-next-line:no-var-requires
+  const { name, version, description } = require('../package.json')
+
+  // tslint:disable:object-literal-sort-keys
+  console.log(
+    commandLineUsage([
+      {
+        header: `${name} ${version}`,
+        content: description,
+      },
+      {
+        header: 'Options',
+        optionList,
+      },
+    ])
+  )
+  // tslint:enable:object-literal-sort-keys
+} else {
+  run()
+}
