1fe01d8f66723e7fe95c7b16173f5c767ac26ad9
Erin Dachtler <erin@erindachtler.me>
2022-10-11 21:37:20 -0700

    tests for duplicate guards

--- tests/generate.ts
+++ tests/generate.ts
@@ -1790,3 +1790,64 @@ testProcessProject(
   },
   { options: { exportAll: true } }
 )
+
+testProcessProject(
+  'guards should only be generated for types that are both exported and annotated in the current file',
+  {
+    'custom.ts': `
+    /** @see {isColor} ts-auto-guard:type-guard */
+    export type Color = 'red' | 'blue' | 'green'`,
+    'index.ts': `
+    export { Color } from './custom'`,
+  },
+  {
+    'custom.ts': null,
+    'index.ts': null,
+    'custom.guard.ts': `
+    /*
+    * Generated type guards for "custom.ts".
+    * WARNING: Do not manually change this file.
+    */
+    import { Color } from "./custom";
+
+    export function isColor(obj: unknown): obj is Color {
+        const typedObj = obj as Color
+        return (
+            (typedObj === "red" ||
+                typedObj === "blue" ||
+                typedObj === "green")
+        )
+    }`,
+  }
+  // { only: true }
+)
+
+testProcessProject(
+  'in exportAll mode, guards should only be generated for types that are exported in the current file',
+  {
+    'custom.ts': `
+    export type Color = 'red' | 'blue' | 'green'`,
+    'index.ts': `
+    export { Color } from './custom'`,
+  },
+  {
+    'custom.ts': null,
+    'index.ts': null,
+    'custom.guard.ts': `
+    /*
+    * Generated type guards for "custom.ts".
+    * WARNING: Do not manually change this file.
+    */
+    import { Color } from "./custom";
+
+    export function isColor(obj: unknown): obj is Color {
+        const typedObj = obj as Color
+        return (
+            (typedObj === "red" ||
+                typedObj === "blue" ||
+                typedObj === "green")
+        )
+    }`,
+  },
+  { /* only: true, */ options: { exportAll: true } }
+)
