079de4985252358fdbe3acd4ad31bfa175276242
Elliot Huffman <ehuffman@elliot-labs.com>
2024-06-04 23:05:19 -0400

    Migrate Linting to Flat File
    Migrate off of the old rc/json config standard to the new flat file config format.
    All settings have been retained from before.
    The node-modules directory doesn't have to be explicitly called out since it is ignored by ESLint by default.

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    'ignores':[
+        'lib',
+        'coverage'
+    ]
+  }
+);
